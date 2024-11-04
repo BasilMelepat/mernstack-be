ae94fd06664b38d165c7cb48263764024d4e6aad
Basil M <98621163+BasilMelepat@users.noreply.github.com>
2024-11-05 00:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    optionsSuccessStatus: 200
+    exposedHeaders: ["set-cookie"]
 }));
 app.use(cookieParser())
 
