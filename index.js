3057839e501aadc3e339992cbf380d42be4ca13a
Basil M <98621163+BasilMelepat@users.noreply.github.com>
2024-11-05 11:55:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.use(express.json());
 app.use(cors({
     origin: ["https://mernstack-fe-green.vercel.app"],
     methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    exposedHeaders: ["set-cookie"]
 }));
 app.use(cookieParser())
 
